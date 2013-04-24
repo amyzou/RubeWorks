228f37bef3c095bbb0f4bb36f7aa15686cba9348
amyzou <azou1@stanford.edu>
2013-04-23 17:39:57 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubeWorks::Application.config.secret_token = 'fad206b4c2a4b03dd4cea9334e81a0c892391256e1e085e5bffeb4c801ecbb15015b3e83e8b953d85ddf6332ccbeec733e8fc47f97558662aef3b934a7c644dc'
