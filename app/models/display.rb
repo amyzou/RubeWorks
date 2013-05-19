97e5220d33d159b86f504aea320591867ba69653
amyzou <azou1@stanford.edu>
2013-05-19 03:18:59 -0700

    Tabs sort by category; undid primary key change

--- app/models/display.rb
+++ app/models/display.rb
@@ -2,7 +2,6 @@ class Display < ActiveRecord::Base
 	# attr_accessible :title, :body
 	attr_accessible :object_id, :obj_file, :texture_file, :image_file, :blocks, :category
 	serialize :blocks 
-	set_primary_key :object_id
 
 	# Needs validation:
 		# Object ID: 	Needs to be unique, and refer to an ID within object table (doesn't exist yet)
@@ -11,4 +10,5 @@ class Display < ActiveRecord::Base
 		# Image File:	Exists in the correct folder (assets/images? perhaps)
 		# Blocks: 		Array of points, each of which are an array of 3 ints.
 		# Category: 	One of the valid categories.
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Categories are: inert/gadget/roamer/carrier
-Display.create(image_file: 'block.png', category:'inert')
-Display.create(image_file: 'dominoes.png', category:'roamer')
-Display.create(image_file: 'fan.png', category:'gadget')
-Display.create(image_file: 'ramp.png', category:'carrier')
-Display.create(image_file: 'sphere.png', obj_file: 'sphere.obj', category:'roamer')
-Display.create(image_file: 'tramp.png', category:'carrier')
+Display.create(object_id: 1, image_file: 'block.png', category:'inert')
+Display.create(object_id: 2, image_file: 'dominoes.png', category:'roamer')
+Display.create(object_id: 3, image_file: 'fan.png', category:'gadget')
+Display.create(object_id: 4, image_file: 'ramp.png', category:'carrier')
+Display.create(object_id: 5, image_file: 'sphere.png', obj_file: 'sphere.obj', category:'roamer')
+Display.create(object_id: 6, image_file: 'tramp.png', category:'carrier')
