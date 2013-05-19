d4f5961e5a7ad84f40cbb379e7ca4b2d993daab6
amyzou <azou1@stanford.edu>
2013-05-19 03:48:17 -0700

    Use partials for displaying tabs.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
   	@displays = Display.all
+  	@inerts = Display.where(:category => 'inert')
+  	@roamers = Display.where(:category => 'roamer')
+  	@gadgets = Display.where(:category => 'gadget')
+  	@carriers = Display.where(:category => 'carrier')
+  end
+
+  def display
+    @display = Display.find(params[:object_id])
   end
 end
