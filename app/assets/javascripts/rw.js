0462a0e0aeb5a8782fbd2cd44ec3e7941850fc6c
amyzou <azou1@stanford.edu>
2013-05-21 00:38:38 -0700

    Call loadObject on click using ajax.

--- app/assets/javascripts/rw.js
+++ app/assets/javascripts/rw.js
@@ -208,8 +208,6 @@ function render() {
 // Dummy function to send object display info
 function loadObject( objectID, objFile, textureFile, block_num, blocks ) {
 	console.log("ObjectID, objFile, textureFile, blocks: " + objectID + ", " + objFile + ", " + textureFile + ", " + block_num + ", " + blocks.toString());
-	if (objectID == 5)
-		console.log(blocks[0]);
 }
 
 // Dummy function to get object ID
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,29 @@
 class HomeController < ApplicationController
   def index
-    @displays = Display.all
-    @inerts = Display.where(:category => 'inert')
-    @roamers = Display.where(:category => 'roamer')
-    @gadgets = Display.where(:category => 'gadget')
-    @carriers = Display.where(:category => 'carrier')
+    @displays   = Display.all
+    @inerts     = Display.where(:category => 'inert')
+    @roamers    = Display.where(:category => 'roamer')
+    @gadgets    = Display.where(:category => 'gadget')
+    @carriers   = Display.where(:category => 'carrier')
   end
 
   def display
-    @display = Display.find(params[:object_id])
+    @display    = Display.find(params[:object_id])
   end
 
   def get_object
-    @object = { "objID" => params['objID'] }
+    @display    = Display.find(params['objID'])
+    @obj_file   = @display.obj_file
+    @tex_file   = @display.texture_file
+    @block_num  = @display.block_num
+    @blocks     = @display.blocks
+
+    @object = { :objID        => params['objID'], 
+                :obj_file     => @obj_file,  
+                :texture_file => @tex_file,
+                :block_num    => @block_num,
+                :blocks       => @blocks
+              }
     render :text => @object.to_json
   end
 end
