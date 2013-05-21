ff68db2f482bde5f80f3c94c97d91c72a5c79a4b
amyzou <azou1@stanford.edu>
2013-05-20 23:20:57 -0700

    Deleted unnecessary obj file; moved arrow images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,18 @@
 class HomeController < ApplicationController
   def index
-  	@displays = Display.all
-  	@inerts = Display.where(:category => 'inert')
-  	@roamers = Display.where(:category => 'roamer')
-  	@gadgets = Display.where(:category => 'gadget')
-  	@carriers = Display.where(:category => 'carrier')
+    @displays = Display.all
+    @inerts = Display.where(:category => 'inert')
+    @roamers = Display.where(:category => 'roamer')
+    @gadgets = Display.where(:category => 'gadget')
+    @carriers = Display.where(:category => 'carrier')
   end
 
   def display
     @display = Display.find(params[:object_id])
+  end
+
+  def get_object
+    @object = { "objID" => params['objID'] }
+    render :text => @object.to_json
   end
 end
