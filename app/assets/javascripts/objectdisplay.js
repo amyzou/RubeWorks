27c713083160c5ccbda07cb15ca82abebb3e7e2a
emilymye <emily3ye@gmail.com>
2013-05-30 14:49:11 -0700

    fix rubeject construction

--- app/assets/javascripts/objectdisplay.js
+++ app/assets/javascripts/objectdisplay.js
@@ -55,36 +55,42 @@ function setCurrentObject ( objectID ) {
 	scene.remove(rollOverMesh);
 	currMeshID = objectID;
 	rollOverMesh = new THREE.Mesh( objectMeshes[objectID].geometry, rollOverMaterial );
-	if (currMeshID == 5) var scale = 25;//objectMeshes[currMeshID].scaleFactor;
-	if (currMeshID == 4) scale = 75/2;
+	var scale = 1;
+	if (currMeshID == 5) 
+		scale = 25;//objectMeshes[currMeshID].scaleFactor;
+	if (currMeshID == 4) {
+		scale = 25 * 1.5;
+	}
 	rollOverMesh.scale = new THREE.Vector3(scale, scale, scale);
-	
-
 	rollOverMesh.position = objectWorldPosition;
 	scene.add(rollOverMesh);
 }
 
 function updateObjectPosition( intersector ) {
+	var offset = new THREE.Vector3(VOXEL_SIZE/2,VOXEL_SIZE/2,VOXEL_SIZE/2);
+	// if (currMeshID == 4) {
+	// 	offset.x = VOXEL_SIZE * 3/2;
+	// 	offset.z = VOXEL_SIZE * .5;
+	// }
 	normalMatrix.getNormalMatrix( intersector.object.matrixWorld );
 	tmpVec.copy( intersector.face.normal );
 	tmpVec.applyMatrix3( normalMatrix ).normalize();
 	objectWorldPosition.addVectors( intersector.point, tmpVec );
 
-	objectWorldPosition.x = Math.floor( objectWorldPosition.x / VOXEL_SIZE ) * VOXEL_SIZE + VOXEL_SIZE/2;
-	objectWorldPosition.y = Math.floor( objectWorldPosition.y / VOXEL_SIZE ) * VOXEL_SIZE + VOXEL_SIZE/2;
-	objectWorldPosition.z = Math.floor( objectWorldPosition.z / VOXEL_SIZE ) * VOXEL_SIZE + VOXEL_SIZE/2;
+	objectWorldPosition.x = Math.floor( objectWorldPosition.x / VOXEL_SIZE ) * VOXEL_SIZE + offset.x;
+	objectWorldPosition.y = Math.floor( objectWorldPosition.y / VOXEL_SIZE ) * VOXEL_SIZE + offset.y;
+	objectWorldPosition.z = Math.floor( objectWorldPosition.z / VOXEL_SIZE ) * VOXEL_SIZE + offset.z;
 	
-	gridPosition[0] = Math.round(objectWorldPosition.x/VOXEL_SIZE + (GRID_SIZE - 1)/2);
-	gridPosition[1] = Math.round(objectWorldPosition.z/VOXEL_SIZE + (GRID_SIZE - 1)/2);
-	gridPosition[2] = Math.round((objectWorldPosition.y - plane.position.y)/VOXEL_SIZE - 0.5);
+	gridPosition[0] = Math.round((objectWorldPosition.x - offset.x)/VOXEL_SIZE + (GRID_SIZE)/2);
+	gridPosition[1] = Math.round((objectWorldPosition.z - offset.z)/VOXEL_SIZE + (GRID_SIZE)/2);
+	gridPosition[2] = Math.round((objectWorldPosition.y - offset.y/VOXEL_SIZE) - 0.5);
 }
 
 function checkGridPosition( pos , block ){
 	console.log(pos);
-	console.log(block);
 	if (pos[0] + block[0] < 0 || pos[0] + block[0] >= GRID_SIZE &&
-		pos[1] + block[1] < 0 || pos[1] + block[1] >= GRID_SIZE &&
-		pos[2] + block[2] < 0 || pos[2] + block[2] >= GRID_HEIGHT)
+		pos[1] + block[2] < 0 || pos[1] + block[2] >= GRID_SIZE &&
+		pos[2] + block[1] < 0 || pos[2] + block[1] >= GRID_HEIGHT)
 			return false;
 
 	return controller.ContainsObject (pos[0], pos[1], pos[2]);
@@ -128,7 +134,7 @@ function addObjectToScene( intersector, intersects ){
 	newMesh.position.copy(objectWorldPosition);
 	scene.add( newMesh );
 	sceneObjects[currSceneID] = newMesh;
-	controller.AddObject(new RubeJect ( currObjPropertyId, gridPosition, false));
+	controller.AddObject(new RubeJect ( currObjPropertyId, gridPosition, currRotation));
 	console.log("added new mesh id = " + currSceneID);
 	currSceneID++;
 }
