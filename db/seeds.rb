670d315fc04bde63552bccd55debe4e7569fec48
Austin Griffith Hulse <ahulse@stanford.edu>
2013-06-06 14:56:01 -0700

    Added flat rails, widened rail column, updated seeds.rb to reflect changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -129,8 +129,8 @@ straightRail = ObjectProperty.create(
     elasticity:             1.0, 
     change_in_height:       0,
     #Relative Animation Information
-    io_map:                 [[[0,0,1,3],[0,0,1,1], "linear", [0,0,1],[1,0,1]],
-                             [[0,0,1,1],[0,0,1,3], "linear", [1,0,1],[0,0,0]]],
+    io_map:                 [[[0,0,0,3],[0,0,0,1], "linear", [0,0,0],[1,0,0]],
+                             [[0,0,0,1],[0,0,0,3], "linear", [1,0,0],[0,0,0]]],
     compatible_roamers:     [5],
 )
 straightRail.display = Display.create(
@@ -151,8 +151,8 @@ lstraightRail = ObjectProperty.create(
     change_in_height:       0,
     #Relative Animation Information
     io_map:                 [
-                            [[0,0,1,3],[2,0,1,1], "linear", [0,0,0],[2,0,1]],
-                            [[2,0,1,1],[0,0,1,3], "linear", [2,0,1],[0,0,0]]
+                            [[0,0,0,3],[2,0,0,1], "linear", [0,0,0],[2,0,0]],
+                            [[2,0,0,1],[0,0,0,3], "linear", [2,0,0],[0,0,0]]
                             ],
     compatible_roamers:     [5],
 )
@@ -186,14 +186,14 @@ shortColumn.updateDisplay
 railRamp = ObjectProperty.create(
     name:                   'Rail Ramp',
     category:               'carrier',
-    block_num:              2,
-    blocks:                 [[0,0,0],[0,0,1]],
+    block_num:              1,
+    blocks:                 [[0,0,0]],
     mass:                   5.0,
     elasticity:             1.0, 
     change_in_height:       1,
     #Relative Animation Information
-    io_map:                 [[[0,0,2,3],[0,0,1,1],"linear",[0,0,2],[1,0,1]],
-                             [[0,0,1,1],[0,0,2,3],"linear",[1,0,1],[0,0,2]]],
+    io_map:                 [[[0,0,1,3],[0,0,0,1],"linear",[0,0,1],[1,0,0]],
+                             [[0,0,0,1],[0,0,1,3],"linear",[1,0,0],[0,0,1]]],
     compatible_roamers:     [5],
 )
 railRamp.display = Display.create(
@@ -213,8 +213,8 @@ turnRail = ObjectProperty.create(
     elasticity:             1.0, 
     change_in_height:       0,
     #Relative Animation Information
-    io_map:                 [[[0,0,1,3],[0,0,1,2], "linear", [0,0,1],[1,0,1]],
-                             [[0,0,1,2],[0,0,1,3], "linear", [1,0,1],[0,0,0]]],
+    io_map:                 [[[0,0,0,3],[0,0,0,2], "linear", [0,0,0],[0,1,0]],
+                             [[0,0,0,2],[0,0,0,3], "linear", [0,1,0],[0,0,0]]],
     compatible_roamers:     [5],
 )
 turnRail.display = Display.create(
